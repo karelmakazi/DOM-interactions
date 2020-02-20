006fa7d6b20904389f6c649083332a40d3047c91
Karel Fourie <karelmakazi@gmail.com>
2020-02-21 10:31:19 +1300

    Add scoreboard functionality

--- game.js
+++ game.js
@@ -41,13 +41,14 @@ function updateCounts () {
     green: 0,
     invisible: 0
   }
+  
+  // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
   for (var key in totals){
     var keyCount = document.getElementsByClassName(key)
     var keyTally = keyCount.length
-    console.log(key + keyTally)
+    totals[key] = keyTally
   }
-  
-  // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+
 
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
